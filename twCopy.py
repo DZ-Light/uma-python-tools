05b202b252564b2e6e20baa6403b2b07d0dc8689
dingzg_area51mr2 <dingziguang@icloud.com>
2022-07-24 02:17:28 +0800

    Update twCopy.py

--- twCopy.py
+++ twCopy.py
@@ -2,9 +2,9 @@ import sqlite3
 from utils import *
 jp_dbPath = "master.mdb"
 tw_dbPath = "tw/master.mdb"
-move_file("C:/Users/dingzg/AppData/LocalLow/Cygames/umamusume/master",
+move_file(os.path.expanduser('~') + "/AppData/LocalLow/Cygames/umamusume/master",
           ".", "master.mdb")
-move_file("C:/Users/dingzg/Documents/leidian/Pictures", "./tw/", "master.mdb")
+move_file(os.path.expanduser('~') + "/Documents/leidian/Pictures", "./tw/", "master.mdb")
 jp_connect = sqlite3.connect(jp_dbPath)
 tw_connect = sqlite3.connect(tw_dbPath)
 print("JP数据库打开成功 === " + jp_dbPath)
